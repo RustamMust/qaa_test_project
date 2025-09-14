3b514a0dcc677a29120e2226b5c7e1d085b1345b
RustamMust <rustammustavaev@gmail.com>
2025-09-14 11:14:10 +0500

    no_task: fix ValidPhoneNumberTest

--- src/test/java/practice_11/homework/task_10/ValidPhoneNumberTest.java
+++ src/test/java/practice_11/homework/task_10/ValidPhoneNumberTest.java
@@ -2,53 +2,27 @@ package practice_11.homework.task_10;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidPhoneNumberTest {
     IsValidPhoneNumber isValidPhoneNumber = new IsValidPhoneNumber();
 
+    @ParameterizedTest
+    @ValueSource(strings = {"+1 1234567890", "+44 9876543210", "+999 1111111111"})
+    public void userCanCheckValidPhoneNumber(String phoneNumber) {
+        boolean actualIsValid = isValidPhoneNumber.isValidPhoneNumber(phoneNumber);
 
-    public static Stream<Arguments> validPhoneNumbers() {
-        return Stream.of(
-                Arguments.of("+1 1234567890", true),
-                Arguments.of("+44 9876543210", true),
-                Arguments.of("+999 1111111111", true)
-
-        );
-    }
-
-    public static Stream<Arguments> inValidPhoneNumbers() {
-        return Stream.of(
-                Arguments.of("12345", false),
-                Arguments.of("invalid", false),
-                Arguments.of("+1 abcdefghij", false),
-                Arguments.of("+1234 1234567890", false),
-                Arguments.of("+1 123", false),
-                Arguments.of("", false)
-
-        );
+        assertTrue(actualIsValid);
     }
 
     @ParameterizedTest
-    @MethodSource("validPhoneNumbers")
-    public void userCanCheckValidPhoneNumber(String phoneNumber, boolean expectedIsValid) {
+    @ValueSource(strings = {"12345", "invalid", "+1 abcdefghij", "+1234 1234567890", "+1 123", ""})
+    public void userCanCheckInValidPhoneNumber(String phoneNumber) {
         boolean actualIsValid = isValidPhoneNumber.isValidPhoneNumber(phoneNumber);
 
-        assertEquals(expectedIsValid, actualIsValid);
-    }
-
-    @ParameterizedTest
-    @MethodSource("inValidPhoneNumbers")
-    public void userCanCheckInValidPhoneNumber(String phoneNumber, boolean expectedIsValid) {
-        boolean actualIsValid = isValidPhoneNumber.isValidPhoneNumber(phoneNumber);
-
-        assertEquals(expectedIsValid, actualIsValid);
+        assertFalse(actualIsValid);
     }
 
     @Test
