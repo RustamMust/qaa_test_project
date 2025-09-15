25a24e7320b857507c5c0ce2d80436c49f534bc2
RustamMust <rustammustavaev@gmail.com>
2025-09-15 18:17:41 +0500

    no_task: add DebugTask10

---
+++ src/main/java/practice_12/homework/DebugTask6.java
@@ -0,0 +1,14 @@
+package practice_12.homework;
+
+public class DebugTask6 {
+    public static void main(String[] args) {
+        countdown(5);
+    }
+    public static void countdown(int n) {
+        System.out.println(n);
+        if (n > 1) {
+            countdown(n - 1);
+        }
+    }
+}
+
